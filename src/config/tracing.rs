a5b58a321d9c94ff907babf5867b525fcf09fa4a
leijendary <jonathanleijendekker@gmail.com>
2023-12-18 17:16:59 +0100

    used import

--- src/config/tracing.rs
+++ src/config/tracing.rs
@@ -1,6 +1,8 @@
+use tracing::Level;
+
 pub fn enable_tracing() {
     tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::INFO)
+        .with_max_level(Level::INFO)
         .with_target(false)
         .without_time()
         .init();
