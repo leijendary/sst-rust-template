298a4e07542160ac2d502095e3adfd08a385341b
leijendary <jonathanleijendekker@gmail.com>
2024-01-02 01:25:08 +0100

    Removed over returning

--- src/domain/sample/repository.rs
+++ src/domain/sample/repository.rs
@@ -163,7 +163,7 @@ impl SampleRepository {
                 last_modified_at = now(),
                 last_modified_by = $6
             where id = $1 and version = $2
-            returning id, name, description, amount, version, created_at, created_by, last_modified_at, last_modified_by";
+            returning id, name, description, amount, version, created_at";
 
         query_as::<_, SampleDetail>(SQL)
             .bind(id)
