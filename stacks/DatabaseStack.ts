43b02b899b240b6cf58f3aead7166453fb68389e
leijendary <jonathanleijendekker@gmail.com>
2024-01-07 22:51:57 +0100

    Changed database config secret to a simple DATABASE_URL

--- stacks/DatabaseStack.ts
+++ stacks/DatabaseStack.ts
@@ -1,11 +1,7 @@
 import { Config, StackContext } from "sst/constructs";
 
 export function DatabaseStack({ stack }: StackContext) {
-  const username = new Config.Secret(stack, "DB_USERNAME");
-  const password = new Config.Secret(stack, "DB_PASSWORD");
-  const url = new Config.Secret(stack, "DB_URL");
-  const name = new Config.Secret(stack, "DB_NAME");
-  const sslMode = new Config.Secret(stack, "DB_SSL_MODE");
+  const url = new Config.Secret(stack, "DATABASE_URL");
 
-  return { username, password, url, name, sslMode };
+  return { url };
 }
